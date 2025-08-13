e3088c55c0bd74211d6d66231b01efabab17a69c
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-08-13 19:15:42 +0530

    react cdn

---
+++ 01-react-cdn/App.js
@@ -0,0 +1,23 @@
+/* <div id="parent">
+  <div id="child">
+    <h1>I'm h1 tag</h1>
+  </div>
+</div> */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm h1 tag")
+  )
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
