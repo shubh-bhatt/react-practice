35c63a84a083a55c908e68d907da177b4a70df83
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-08-17 19:58:41 +0530

    restaurant filteration by rating applied

--- 01-mamas-food/src/components/Body.js
+++ 01-mamas-food/src/components/Body.js
@@ -1,12 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantData } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [resData, setResData] = useState(restaurantData);
+
   return (
     <div className="body">
       <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-button"
+          onClick={() => {
+            const filteredList = resData.filter(
+              (restaurant) => restaurant.info.avgRating > 4
+            );
+            setResData(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
-        {restaurantData.map((restaurant) => (
+        {resData.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} restaurant={restaurant} />
         ))}
       </div>
