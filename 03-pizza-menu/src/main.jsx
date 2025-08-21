e230619ccfcf522a7cc899634e7478daeb26297a
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-08-22 00:56:45 +0530

    list updated

--- 03-pizza-menu/src/main.jsx
+++ 03-pizza-menu/src/main.jsx
@@ -82,14 +82,14 @@ function Pizza(props) {
   console.log(props);
 
   return (
-    <div className="pizza">
+    <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
         <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
