49544821e2491c613a9a32240b903804fe6fe67c
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-11 19:16:32 +0530

    Dark theme added

--- counter-hooks/src/App.jsx
+++ counter-hooks/src/App.jsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [mode, setMode] = useState("Dark");
+
+  const styles =
+    mode === "Dark"
+      ? { color: "white", backgroundColor: "black" }
+      : { color: "black", backgroundColor: "white" };
+
+  const toggleTheme = () => {
+    setMode((prev) => (prev === "Dark" ? "Light" : "Dark"));
+  };
+
   return (
-    <div className="main-container">
+    <div className="main-container" style={styles}>
       <p>{counter}</p>
       <button onClick={() => setCounter((prev) => prev + 1)}>Increase</button>
       <button
@@ -15,6 +26,7 @@ const App = () => {
         Decrease
       </button>
       <button onClick={() => setCounter(0)}>Reset</button>
+      <button onClick={toggleTheme}>{mode} Mode</button>
     </div>
   );
 };
