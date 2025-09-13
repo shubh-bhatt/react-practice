eeb10f99745686a15a5c8f19c9a1c0ce4a9d0286
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-13 13:46:57 +0530

    page link

--- 01-mamas-food/src/components/Header.jsx
+++ 01-mamas-food/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [login, setLogin] = useState("Login");
@@ -11,10 +12,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>Products</li>
-          <li>About Us</li>
-          <li>Contact</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login-button"
