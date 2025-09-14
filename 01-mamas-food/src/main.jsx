67464e9f666a23d1a05aee1438091a6d9f862fa0
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-14 21:33:37 +0530

    project removed

--- 01-mamas-food/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import Routes from "./routes/Routes";
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={Routes} />
-);
--- 01-mamas-food/src/utils/constants.jsx
+++
@@ -1,5 +0,0 @@
-export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL =
-  "https://marketplace.canva.com/EAGXsRURT9o/1/0/1600w/canva-yellow-and-brown-kitchen-food-logo-JrzZUwc_CLQ.jpg";
